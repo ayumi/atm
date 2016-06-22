125c5fa0d1f3fa633424b230ae503e5f8842f04b
Ayumi Yu <ayumi@ayumiyu.com>
2016-06-21 23:23:42 -0700

    Basic ui

---
+++ README.md
@@ -0,0 +1,1 @@
+screen dimensions: 800x480
---
+++ index.js
@@ -0,0 +1,9 @@
+/*eslint-env es6 */
+class Atm {
+  constructor (args) {
+    this.args = args;
+    // this.init();
+  }
+}
+
+window.atm = new Atm({cute: 123});
