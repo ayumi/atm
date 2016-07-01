459f6357560afb1264e191bc1ed6a2f4bafa68c4
Ayumi Yu <ayumi@ayumiyu.com>
2016-06-30 21:59:43 -0700

    More bible

--- bible.js
+++ bible.js
@@ -1,9 +1,87 @@
 Atm.bible = [
-  '<h2>{Numbers 31:32}</h2><p>{These were the spoils which remained of the plunder taken by the fighting men: 675,000 sheep, 72,000 cattle, 61,000 donkeys, and as for persons, 32,000 young women who had had no intercourse with a man.}</p>',
   '<h2>{Genesis 15:9}</h2><p>{The Lord answered, “Bring me a heifer three years old, a she-goat, three years old, a ram three years old, a turtle dove and a young pigeon.”}</p>',
+
   '<h2>{Deuteronomy 25:11}</h2><p>{When two men are fighting and the wife of one of them intervenes to drag her husband clear of his opponent, if she puts out her hand and catches hold of the man by his privates, you must cut off her hand and show her no mercy.}</p>',
+
   '<h2>{Deuteronomy 28:53}</h2><p>{Then because of the dire straits to which you will be reduced when your enemy besieges you, you will eat your own children, the flesh of your sons and daughters whom the Lord has given you.}</p>',
+
   '<h2>{Judges 3:21}</h2><p>{And Ehud reached with his left hand, took the sword from his right thigh, and thrust it into his belly. And the hilt also went in after the blade, and the fat closed over the blade, for he did not pull the sword out of his belly; and the dung came out.}',
+
   '<h2>{Mark 14:51}</h2><p>{A young man was following Him, wearing nothing but a linen sheet over his naked body; and they seized him.}</p><p>{But he pulled free of the linen sheet and escaped naked.}</p>',
-  '<h2>{Ezekiel 16:17}</h2><p>{You also took the fine jewelry I gave you, the jewelry made of my gold and silver, and you made for yourself male idols and engaged in prostitution with them.}</p>'
+
+  '<h2>{Ezekiel 16:17}</h2><p>{You also took the fine jewelry I gave you, the jewelry made of my gold and silver, and you made for yourself male idols and engaged in prostitution with them.}</p>',
+
+  '<h2>{Ezekiel 23:19}</h2><p>{Yet she increased her prostitution, remembering the days of her youth when she engaged in prostitution in the land of Egypt. She lusted after their genitals – as large as those of donkeys, and their seminal emission was as strong as that of stallions.}</p>',
+
+  '<h2>{Deuteronomy 28:56}</h2><p>{The most refined and delicate woman will refuse her husband and children her afterbirth and infant, that comes out from between her legs, as she secretly eats them for food.}</p>',
+
+  '<h2>{Exodus 22:17}</h2><p>{You should not let a sorceress live.}</p>',
+
+  '<h2>{Leviticus 20:27}</h2><p>{A man or a woman who acts as a medium or fortuneteller shall be put to death by stoning; they have no one but themselves to blame for their death.}</p>',
+
+  '<h2>{Deuteronomy 20:10}</h2><p>{As you approach a town to attack it, first offer its people terms for peace. If they accept your terms and open the gates to you, then all the people inside will serve you in forced labor.}</p><p>{But if they refuse to make peace and prepare to fight, you must attack the town. When the LORD your God hands it over to you, kill every man in the town.}</p><p>{But you may keep for yourselves all the women, children, livestock, and other plunder. You may enjoy the spoils of your enemies that the LORD your God has given you.}</p>',
+
+  '<h2>{Deuteronomy 23:1}</h2><p>{No one whose testicles are crushed or whose male organ is cut off shall enter the assembly of the Lord.}</p>',
+
+  '<h2>{Leviticus 19:19}</h2><p>{Do not wear clothing woven of two kinds of material.}</p>',
+
+  '<h2>{Matthew 21:18-22}</h2><p>{Early in the morning, as Jesus was on his way back to the city, he was hungry. Seeing a fig tree by the road, he went up to it but found nothing on it except leaves. Then he said to it, “May you never bear fruit again!” Immediately the tree withered.}</p>',
+
+  '<h2>{Matthew 5:11-12}</h2><p>{Blessed are you when people insult you, persecute you and falsely say all kinds of evil against you because of me.}</p>',
+
+  '<h2>{Luke 19:23-26}</h2><p>{"Take the money from this servant, and give it to the one who has ten pounds."}</p><p>{"But, master," they said, "he already has ten pounds!"}</p><p>"Yes", the king replied, "and to those who use well what they are given, even more will be given. But from those who do nothing, even what little they have will be taken away."}</p>',
+
+  '<h2>{Matthew 5:39}</h2><p>{But I tell you, do not resist an evil person. If anyone slaps you on the right cheek, turn to them the other cheek also.}</p>',
+
+  '<h2>{Mark 2:23-28}</h2><p>{One Sabbath Jesus was going through the grainfields, and as his disciples walked along, they began to pick some heads of grain.}</p><p>{The Pharisees said to him, "Look, why are they doing what is unlawful on the Sabbath?"}</p>',
+
+  '<h2>{Matthew 18:9}</h2><p>{And if thine eye offend thee, pluck it out, and cast it from thee: it is better for thee to enter into life with one eye, rather than having two eyes to be cast into hell fire.}</p>',
+
+  '<h2>{Mark 5:13}</h2><p>{And all the devils besought him, saying, Send us into the swine, that we may enter into them.}</p><p>{And forthwith Jesus gave them leave. And the unclean spirits went out, and entered into the swine: and the herd ran violently down a steep place into the sea, (they were about two thousand;) and were choked in the sea.}</p>',
+
+  '<h2>{Mark 14:22-25}</h2><p>{And as they did eat, Jesus took bread, and blessed, and brake it, and gave to them, and said, Take, eat: this is my body.}</p>',
+
+  '<h2>{2 Peter 2:5}</h2><p>{And spared not the old world, but saved Noah the eighth person, a preacher of righteousness, bringing in the flood upon the world of the ungodly}</p>',
+
+  '<h2>{Genesis 25:30}</h2><p>{He said to Jacob, "Let me eat some of that red stuff, because I\'m exhausted."}</p>',
+
+  '<h2>{Proverbs 21:19}</h2><p>{It is better to live alone in the desert than with a crabby, complaining wife.}</p>',
+
+  '<h2>{Proverbs 21:19}</h2><p>{He turned around, and looked at them and called down a curse on them in the name of the Lord.}</p><p>{Then two bears came out of the woods and mauled forty-two of the youths.}</p>',
+
+  '<h2>{Proverbs 31:6}</h2><p>{Give beer to those who are perishing, wine to those who are in anguish.}</p>',
+
+  '<h2>{Acts 20:9}</h2><p>{As Paul spoke on and on, a young man named Eutychus, sitting on the windowsill, became very drowsy.}</p><p>{Finally, he fell sound asleep and dropped three stories to his death below.}</p>',
+
+  '<h2>{Deuteronomy 14:21}</h2><p>{Do not eat anything you find already dead. You may give it to an alien living in any of your towns and he may eat it, or you may sell it to a foreigner.}</p>',
+
+  '<h2>{Judges 16:17}</h2><p>{"No razor has ever been used on my head," he said, "because I have been a Nazirite set apart to God since birth. If my head were shaved, my strength would leave me, and I would become as weak as any other man."}</p>',
+
+  '<h2>{Ezekiel 4:15}</h2><p>{“Very well,” [God] said, “I will let you bake your bread over cow dung instead of human excrement.”}</p>',
+
+  '<h2>{Luke 12:49}</h2><p>{I have come to set the world on fire, and I wish it were already burning!}</p><p>{- Jesus}</p>',
+
+  '<h2>{Song of Soloman 4:5}</h2><p>{Your breasts are like two fawns, like twin fawns of a gazelle that browse among the lilies.}</p>',
+
+  '<h2>{Ecclesiastes 1:2}</h2><p>{Everything is meaningless.}</p>',
+
+  '<h2>{Ecclesiastes 9:9}</h2><p>{Live happily with the woman you love through all the meaningless days of life.}</p>',
+
+  '<h2>{Matthew 10:34}</h2><p>{Do not suppose that I have come to bring peace to the earth. I did not come to bring peace, but a sword.}</p><p>-Jesus</p>',
+
+  '<h2>{Psalm 137:9}</h2><p>{Happy is the man who takes and dashes your infacnts against the rock.}</p>',
+
+  '<h2>{Exodus 4:24-26}</h2><p>{At a lodging place on the way, the Lord met Moses and was about to kill him. But Zipporah took a flint knife, cut off her son’s foreskin and touched Moses’ feet with it. ‘Surely you are a bridegroom of blood to me,’ she said. So the Lord let him alone.}</p>',
+
+  '<h2>{Proverbs 20:30}</h2><p>{“Lashes and wounds purge away evil, and beatings cleanse the innermost parts.}</p>',
+
+  '<h2>{Leviticus 15:16}</h2><p>{Whenever a man has an emission of semen, he must bathe his entire body in water, and he will remain ceremonially unclean until the next evening.}</p>',
+
+  '<h2>{Deuteronomy 5:9}</h2><p>{I, the LORD your God, am a jealous God, punishing the children for the sin of the parents to the third and fourth generation of those who hate me.}</p>',
+
+  '<h2>{1 Samuel 18:27}</h2><p>{David and his men went out and killed 200 Philistines. He brought their foreskins and presented them as full payment to the king to become his son-in-law.}</p><p>{Then Saul gave his daughter Michal to David as his wife.}</p>',
+
+  '<h2>{Genesis 6:6-7}</h2><p>{The LORD regretted that he had made human beings on the earth, and his heart was deeply troubled. So the LORD said, ‘I will wipe from the face of the earth the human race I have created–and with them the animals, the birds and the creatures that move along the ground–for I regret that I have made them.’}</p>',
+
+  '<h2>{Isaiah 45:7}</h2><p>{I form the light, and create darkness: I make peace, and create evil: I the LORD do all these things.}</p>'
 ];
